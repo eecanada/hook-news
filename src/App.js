a4e1206c642920c05e5a3a1218809b90e4679eab
Eder Canada <canada.eder@gmail.com>
2022-02-18 11:38:04 -0800

    spinner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useRef } from 'react';
 import axios from 'axios';
-
+import { Spinner, Button } from 'react-bootstrap';
 function App() {
   const [hits, setHits] = useState([]);
   const [search, setSearch] = useState('react hooks');
+  const [loading, setLoading] = useState(false);
+  const ref = useRef();
 
   // useEffect(async () => {}, []);
 
@@ -11,28 +13,40 @@ function App() {
     setSearch(e.target.value);
   };
 
+  const handleClear = () => {
+    setSearch('');
+    if (ref.current) ref.current.focus();
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     const results = await axios.get(
       `http://hn.algolia.com/api/v1/search?query=${search}`
     );
     setHits(results.data.hits);
+    setSearch('');
+    setLoading(false);
   };
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
         <input
+          ref={ref}
           type="text"
           placeholder="search"
           value={search}
           onChange={handleSearchChange}
         />
+        <button onClick={handleClear}>Clear</button>
         <button>Seach</button>
       </form>
 
-      {!search ? (
-        <p> nothing to see here folks</p>
+      {loading ? (
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
       ) : (
         <ul>
           {hits.map((hit) => {
