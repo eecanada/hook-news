58c871ca8b8564e1498204900e2afc26795fcd58
Eder Canada <canada.eder@gmail.com>
2022-02-17 16:01:37 -0800

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [hits, setHits] = useState([]);
+  
+  useEffect(async () => {
+    const results = await axios.get(
+      'http://hn.algolia.com/api/v1/search?query=reacthooks'
+    );
+    setHits(results.data.hits);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {hits.map((hit) => {
+          return (
+            <li key={hit.objectID}>
+              <a href={hit.url}>{hit.title}</a>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
